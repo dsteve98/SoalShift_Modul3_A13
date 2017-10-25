bcfb372eff0520dc7f11e0776d3c9db049a3867e
dsteve98 <dan.steve2910@gmail.com>
2017-10-25 11:53:04 +0700

    soal1 coba strcmp

--- soal1_owner.c
+++ soal1_owner.c
@@ -2,22 +2,47 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <string.h>
 
 void main()
 {
-        key_t key = 1234;
+    key_t key = 1234;
+    int choice;
+    int it;
+    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+    int *senjata = shmat(shmid, NULL, 0);
+    char nama[7][10]={"MP4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE","\0"};
 
-        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-        int *senjata = shmat(shmid, NULL, 0);
+    senjata[0] = 0;
+    senjata[1] = 0;
+    senjata[2] = 0;
+    senjata[3] = 0;
+    senjata[4] = 0;
+    senjata[5] = 0;
 
-        senjata[0] = 10;
-        senjata[1] = 20;
+    printf("0.Exit\n1.List\n2.Supply\n");
 
-        printf("%d--%d\n", senjata[0], senjata[1]);
+    while(scanf("%d",&choice)){
+        if(choice==0) break;
+        if(choice==1){
+            for(it=0;it<6;it++){
+            printf("%s\t%d\n",nama[it],senjata[it]);
+            }
+        }
+        else if(choice==2){
+            char comp[10];
+            int total;
+            scanf("%s%d",comp,&total);
+            for(it=0;it<6;it++){
+                if(!strcmp(comp,nama[it])){
+                senjata += total;
+                break;
+                }
+            }
+        }
 
-        sleep(5);
+    }
 
-        printf("%d--%d\n", senjata[0], senjata[1]);
-        shmdt(senjata);
-        shmctl(shmid, IPC_RMID, NULL);
+    shmdt(senjata);
+    shmctl(shmid, IPC_RMID, NULL);
 }
