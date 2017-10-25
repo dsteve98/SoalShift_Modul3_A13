845aea86eddc9a9ff171105e2f137b591230dba2
dsteve98 <dan.steve2910@gmail.com>
2017-10-25 13:23:44 +0700

    soal3 success

--- soal3.c
+++ soal3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h> //library thread
 #include <sys/wait.h>
+#include <unistd.h>
 
 
 int flag = 0;
@@ -10,16 +11,49 @@ int statkepiting = 100;
 
 void *Lohan()
 {
-     
+    while(flag==0){
+        statlohan -= 15;
+        if(statlohan<=0||statlohan>100||statkepiting<=0||statkepiting>100||flag==1){
+            flag=1;
+            break;
+        }
+        sleep(10);
+    }
 }
 void *Kepiting()
 {
-     
+     while(flag==0){
+        statkepiting -= 10;
+        if(statlohan<=0||statlohan>100||statkepiting<=0||statkepiting>100||flag==1){
+            flag=1;
+            break;
+        }
+        sleep(20);
+    }
 }
+void *Makan()
+{
+    int choice;
+    while(flag==0){
+        printf("********************\n");
+        printf("Lohan\t%d\nKepiting\t%d\n",statlohan,statkepiting);
+        printf("1 for lohan, 2 for kepiting\n");
+        scanf("%d",&choice);
+        if(choice==1) statlohan += 10;
+        else if(choice==2) statkepiting += 10;
+
+        if(statlohan<=0||statlohan>100||statkepiting<=0||statkepiting>100||flag==1){
+            flag=1;
+            break;
+        }
+    }
+}
+
+
 int main()
 {
-     pthread_t thread1, thread2;//inisialisasi awal
-	int  iret1, iret2;
+     pthread_t thread1, thread2, thread3;//inisialisasi awal
+	int  iret1, iret2, iret3;
 
      iret1 = pthread_create( &thread1, NULL, Lohan, NULL);
      if(iret1)//jika gagal
@@ -36,9 +70,21 @@ int main()
          fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
          exit(EXIT_FAILURE);
      }
-	pthread_join( thread1, NULL);
-	pthread_join( thread2, NULL);
+	
+     iret3 = pthread_create( &thread3, NULL, Makan, NULL);
+     if(iret3)//jika gagal
+     {
+	 
+         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+         exit(EXIT_FAILURE);
+     }
 
+
+    pthread_join( thread1, NULL);
+	pthread_join( thread2, NULL);
+    pthread_join( thread3, NULL);
+    
+    printf("GAME OVER\n");
      //
      // 
 
