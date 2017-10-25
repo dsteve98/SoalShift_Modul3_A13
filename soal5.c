abad47afd0a5774d0c4e5edcd44f0c98a352e1df
Fariz <fariz.ardin@gmail.com>
2017-10-25 17:05:52 +0700

    Edit lagi

--- soal5.c
+++ soal5.c
@@ -4,7 +4,19 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+void *print_message_function( void *ptr );
+
 int main(int argc, char *argv[]){
-printf("%d",argc);
-for(int i=1;i<argc;i++)printf("%s",argv[i]);
+pthread_t thread1, thread2;
+const char *message1 = argv[1];
+const char *message2 = argv[2];
+int iret1, iret2;
+
+iret = pthread_create(&thread1,NULL, print_message_function, (void*) message1);
+if(iret1)
+{
+    fprintf(stderr,"Error - pthread_create() return code : %d\n",iret1);
+    exit(EXIT_FAILURE);
+}
+
 }
