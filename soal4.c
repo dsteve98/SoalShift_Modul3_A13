9ea5b88447e87b20048721ee66999615f01076e4
dsteve98 <dan.steve2910@gmail.com>
2017-10-25 17:58:05 +0700

    soal4 -masihfail

--- soal4.c
+++ soal4.c
@@ -4,13 +4,24 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+int ans;
+void* factorial(void* fac){
+    int fct=(int)fac;
+    for(int a=1;a<=fct;a++) 
+    {
+    ans=ans*a;
+    printf("Hasil %d! = %d\n",fct, ans);
+    }
+}
+
 int main(int argc, char *argv[]){
     pthread_t thread[argc];
-    int num[argc];
-    for(int i=1;i<argc;i++){
-    num[i] = atoi(argv[i]);
+    int num;
+    for(int i=1;i<argc-1;i++){
+        num = atoi(argv[i]);
+        pthread_create(thread[i],NULL,&factorial,(void *)num);
     }
-    for(int i=1;i<argc;i++){
-    printf("%d\n",num[i]);
+    for(int i=1;i<argc-1;i++){
+        pthread_join(thread[i],NULL);
     }
 }
